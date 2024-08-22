5f67fedbb78bfba12852923b6691314a276aef52
Godwin Opara <godwinopara62@yahoo.com>
2024-08-22 04:07:59 +0100

    made the application mobile responsive

--- src/pages/CountryDetails.tsx
+++ src/pages/CountryDetails.tsx
@@ -18,44 +18,52 @@ export default function CountryDetails() {
   const { flags, name, population, region, capital, currencies, languages, timezones } = country;
 
   return (
-    <section className="max-w-7xl mx-auto min-h-screen flex gap-x-10 items-center justify-center text-lg">
-      <img src={flags.png} alt={`Flag of ${name.common}`} className="w-[40%]" />
-      <div className="w-[50%]">
-        <div className="mb-8 flex items-center justify-between">
-          <h2 className="font-bold text-4xl uppercase ">{name.common}</h2>
-          <Link to="/" className="bg-black text-white text-lg py-2 px-8">
-            Back Home
-          </Link>
+    <>
+      <section className="max-w-7xl mx-auto min-h-screen px-6 lg:px-0 lg:flex items-center justify-center text-lg">
+        <div>
+          <div className=" max-w-7xl mx-auto py-10">
+            <Link to="/" className="bg-black text-white text-lg py-2 px-8 inline-block">
+              Back Home
+            </Link>
+          </div>
+          <div className="grid lg:grid-cols-2 gap-x-10">
+            <img src={flags.png} alt={`Flag of ${name.common}`} className="w-full mb-8 lg:mb-0" />
+            <div>
+              <div className="mb-8">
+                <h2 className="font-bold text-4xl uppercase ">{name.common}</h2>
+              </div>
+              <p className="flex gap-x-6 items-center mb-2">
+                <span className="font-semibold text-xl">Population:</span>
+                {population.toLocaleString()} {/* Format population with commas */}
+              </p>
+              <p className="flex gap-x-6 items-center mb-2">
+                <span className="font-semibold text-xl">Region:</span>
+                {region}
+              </p>
+              <p className="flex gap-x-6 items-center mb-2">
+                <span className="font-semibold text-xl">Capital:</span>
+                {capital.join(", ")} {/* Join capital array into a string */}
+              </p>
+              <p className="flex gap-x-6 items-center mb-2">
+                <span className="font-semibold text-xl">Currencies:</span>
+                {Object.entries(currencies).map(([code, currency]) => (
+                  <span key={code}>
+                    {currency.name} ({currency.symbol})
+                  </span>
+                ))}
+              </p>
+              <p className="flex gap-x-6 items-center mb-2">
+                <span className="font-semibold text-xl">Languages:</span>
+                {Object.values(languages).join(", ")} {/* Join multiple languages with commas */}
+              </p>
+              <p className="flex gap-x-6 items-center mb-2">
+                <span className="font-semibold text-xl">Timezones:</span>
+                {timezones.join(", ")} {/* Join multiple timezones with commas */}
+              </p>
+            </div>
+          </div>
         </div>
-        <p className="flex gap-x-6 items-center mb-2">
-          <span className="font-semibold text-xl">Population:</span>
-          {population.toLocaleString()} {/* Format population with commas */}
-        </p>
-        <p className="flex gap-x-6 items-center mb-2">
-          <span className="font-semibold text-xl">Region:</span>
-          {region}
-        </p>
-        <p className="flex gap-x-6 items-center mb-2">
-          <span className="font-semibold text-xl">Capital:</span>
-          {capital.join(", ")} {/* Join capital array into a string */}
-        </p>
-        <p className="flex gap-x-6 items-center mb-2">
-          <span className="font-semibold text-xl">Currencies:</span>
-          {Object.entries(currencies).map(([code, currency]) => (
-            <span key={code}>
-              {currency.name} ({currency.symbol})
-            </span>
-          ))}
-        </p>
-        <p className="flex gap-x-6 items-center mb-2">
-          <span className="font-semibold text-xl">Languages:</span>
-          {Object.values(languages).join(", ")} {/* Join multiple languages with commas */}
-        </p>
-        <p className="flex gap-x-6 items-center mb-2">
-          <span className="font-semibold text-xl">Timezones:</span>
-          {timezones.join(", ")} {/* Join multiple timezones with commas */}
-        </p>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
